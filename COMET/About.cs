cb9dee0eaab8785b5ad5932e593949a9041582dd
JaredSimon <JaredSimon@CARBONXIII>
2026-03-17 01:02:22 -0700

    Updated with Copilot

--- COMET/About.cs
+++ COMET/About.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Comet1.Properties;
 
 namespace Comet1
 {
@@ -22,7 +23,8 @@ namespace Comet1
         public About()
         { 
             InitializeComponent();
-            labelVersion.Text = Application.ProductVersion;
+            // Show assembly product/version and generated build info
+            labelVersion.Text = BuildInfo.Version + " (built " + BuildInfo.BuildDateTime + ")";
             labelName.Text = Application.ProductName;
             labelPath.Text = Application.ExecutablePath;
             labelDevelopers.Text = "J.Simon";
@@ -42,7 +44,7 @@ namespace Comet1
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(234, 85);
+            this.button1.Location = new System.Drawing.Point(337, 85);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -55,7 +57,7 @@ namespace Comet1
             this.labelName.AutoSize = true;
             this.labelName.Location = new System.Drawing.Point(12, 13);
             this.labelName.Name = "labelName";
-            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.Size = new System.Drawing.Size(44, 16);
             this.labelName.TabIndex = 1;
             this.labelName.Text = "Name";
             // 
@@ -64,7 +66,7 @@ namespace Comet1
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(11, 59);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.Size = new System.Drawing.Size(96, 16);
             this.label2.TabIndex = 2;
             this.label2.Text = "Developed by:";
             // 
@@ -73,7 +75,7 @@ namespace Comet1
             this.labelDevelopers.AutoSize = true;
             this.labelDevelopers.Location = new System.Drawing.Point(106, 59);
             this.labelDevelopers.Name = "labelDevelopers";
-            this.labelDevelopers.Size = new System.Drawing.Size(47, 13);
+            this.labelDevelopers.Size = new System.Drawing.Size(58, 16);
             this.labelDevelopers.TabIndex = 3;
             this.labelDevelopers.Text = "J. Simon";
             // 
@@ -82,7 +84,7 @@ namespace Comet1
             this.labelVersion.AutoSize = true;
             this.labelVersion.Location = new System.Drawing.Point(106, 13);
             this.labelVersion.Name = "labelVersion";
-            this.labelVersion.Size = new System.Drawing.Size(75, 13);
+            this.labelVersion.Size = new System.Drawing.Size(93, 16);
             this.labelVersion.TabIndex = 4;
             this.labelVersion.Text = "currentVersion";
             // 
@@ -91,7 +93,7 @@ namespace Comet1
             this.labelPath.AutoSize = true;
             this.labelPath.Location = new System.Drawing.Point(12, 35);
             this.labelPath.Name = "labelPath";
-            this.labelPath.Size = new System.Drawing.Size(28, 13);
+            this.labelPath.Size = new System.Drawing.Size(33, 16);
             this.labelPath.TabIndex = 5;
             this.labelPath.Text = "path";
             // 
@@ -99,7 +101,7 @@ namespace Comet1
             // 
             this.button2.Location = new System.Drawing.Point(13, 84);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(140, 23);
+            this.button2.Size = new System.Drawing.Size(140, 24);
             this.button2.TabIndex = 6;
             this.button2.Text = "Send Bug Report";
             this.button2.UseVisualStyleBackColor = true;
@@ -107,7 +109,7 @@ namespace Comet1
             // 
             // About
             // 
-            this.ClientSize = new System.Drawing.Size(321, 120);
+            this.ClientSize = new System.Drawing.Size(424, 120);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.labelPath);
             this.Controls.Add(this.labelVersion);
